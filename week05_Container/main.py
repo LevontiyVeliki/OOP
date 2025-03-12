ccd48bbb5a9564968605e6c6d5b7145fd4e76f65
LevontiyVeliki <151672905+LevontiyVeliki@users.noreply.github.com>
2025-03-12 12:01:37 +0300

    Add files via upload

--- week05_Container/main.py
+++ week05_Container/main.py
@@ -1,10 +1,8 @@
-from Collector import MyDateTime
+from MyDateTime import MyDateTime
 from Collector import DateTimeCollection
 
 dt = MyDateTime(2010, 10, 10, 20, 31, 0)
 dt_mas = DateTimeCollection()
-
-MyDateTime.from_datetime()
 
 dt_mas.add(dt)
 
